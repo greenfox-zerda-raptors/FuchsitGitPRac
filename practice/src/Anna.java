6b480fdaa858cb68325063f9183fbd785cc377f1
zsofinnemeth <zsofinnemeth@gmail.com>
2017-01-15 18:24:50 +0100

    d6

--- practice/src/Anna.java
+++ practice/src/Anna.java
@@ -3,7 +3,7 @@
  */
 public class Anna {
 
-    public void cheer(String teamName) {
-        System.out.println("javajava");
+    public void cheer(String name) {
+        System.out.println("Cheer for the" + name);
     }
 }
